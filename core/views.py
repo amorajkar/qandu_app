1c2009efeec5fa5e0eb9bcb8d6ff4fd67682f40f
amorajkar <amorajkar@gmail.com>
2015-10-27 15:37:11 +0000

    Add create view

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from .models import *
 
 # Create your views here.
 class Home(TemplateView):
-    template_name = "home.html"
+  template_name = "home.html"
 
 class QuestionCreateView(CreateView):
     model = Question
@@ -14,8 +14,8 @@ class QuestionCreateView(CreateView):
     success_url = reverse_lazy('question_list')
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(QuestionCreateView, self).form_valid(form)
+      form.instance.user = self.request.user
+      return super(QuestionCreateView, self).form_valid(form)
 
 class QuestionListView(ListView):
     model = Question
@@ -34,3 +34,16 @@ class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'question/question_confirm_delete.html'
     success_url = reverse_lazy('question_list')
+
+class AnswerCreateView(CreateView):
+    model = Answer
+    template_name = "answer/answer_form.html"
+    fields = ['text']
+
+    def get_success_url(self):
+        return self.object.question.get_absolute_url()
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.question = Question.objects.get(id=self.kwargs['pk'])
+        return super(AnswerCreateView, self).form_valid(form)
